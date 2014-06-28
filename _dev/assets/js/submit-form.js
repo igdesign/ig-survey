88481a423a7deeedc342209fec9221661d7fb651
Gregory Daynes <gregdaynes@gmail.com>
2014-06-27 17:25:25 -0700

    connect to the server

--- _dev/assets/js/submit-form.js
+++ _dev/assets/js/submit-form.js
@@ -1,43 +1,45 @@
 /**
  * Submit Form
  */
+var form = document.getElementById("entryForm");
 
-// on submit form
-// check form for required fields
-// collect fields for submission
-// send data to server
-// wait for response
-// if response >= 200 < 400
-//   display message
-// if < 400
-//   display error
-
-
-
-/*
-request = new XMLHttpRequest();
-request.open('GET', '/my/url', true);
-
-request.onload = function() {
-  if (request.status >= 200 && request.status < 400){
-    // Success!
-    data = JSON.parse(request.responseText);
-  } else {
-    // We reached our target server, but it returned an error
-
+form.addEventListener("submit", function(e)
+{
+	e.preventDefault();
+  var f = e.target,
+      formData = '';
+	// fetch form values
+  for (var i = 0, d, v; i < f.elements.length; i++) {
+      d = f.elements[i];
+      if (d.name && d.value) {
+          v = (d.type == "checkbox" || d.type == "radio" ? (d.checked ? d.value : '') : d.value);
+          if (v) formData += d.name + "=" + escape(v) + "&";
+      }
   }
-};
 
-request.onerror = function() {
-  // There was a connection error of some sort
-};
 
-request.send();
-*/
+	request = new XMLHttpRequest();
+  request.open('POST', 'http://php.igdesign.ca/survey/', true);
+  request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
 
-/*
-var request = new XMLHttpRequest();
-request.open('POST', '/my/url', true);
-request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-request.send(data);
-*/
+
+  request.onload = function() {
+    if (request.status >= 200 && request.status < 400) {
+      // Success!
+
+        data = JSON.parse(request.responseText);
+        console.log(data);
+        window.location.href = "/thank-you.html";
+    } else {
+      // We reached our target server, but it returned an error
+      console.log(response);
+    }
+  };
+
+  request.onerror = function() {
+    // There was a connection error of some sort
+    console.log('error');
+  };
+
+  request.send(formData);
+});
